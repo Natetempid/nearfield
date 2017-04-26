4b997dfbe9cd621497443d8783fbb1b9badd07ad
Nathan Thomas <nthomas@caltech.edu>
2017-04-25 23:03:46 -0700

    20170425_2302

--- TestVisa/TestVisa/TestVisa.py
+++ TestVisa/TestVisa/TestVisa.py
@@ -1,2 +1,9 @@
-import visa
+import pyvisa
 
+rm = pyvisa.ResourceManager()
+#print rm.list_resources()
+lkshr = rm.open_resource("ASRL3::INSTR")
+lkshr.baud_rate = 57600
+lkshr.parity = lkshr.parity.odd
+lkshr.data_bits = 7
+print lkshr.query("*IDN?")
