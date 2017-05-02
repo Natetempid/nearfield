bb3b4f42169bc1f0b7824476537b13204b864e54
Nathan Thomas <nthomas@caltech.edu>
2017-05-02 10:45:11 -0700

    20170502_1044

--- TestTkInter/TestTkInter/TestTkInter.py
+++ TestTkInter/TestTkInter/TestTkInter.py
@@ -5,26 +5,40 @@ from matplotlib.figure import Figure
 import matplotlib.animation as animation
 
 import Tkinter as tk
+import ttk
 from instruments import lakeshore335
 import threading
 import datetime
+import numpy as np
 
 lkshr = lakeshore335('ASRL3::INSTR')
 
 LARGE_FONT= ("Verdana", 12)
     
-f = Figure(figsize=(5,5), dpi = 100)
-a = f.add_subplot(111)
+f = Figure(figsize=(10,5), dpi = 100)
+aA = f.add_subplot(121)
+aB = f.add_subplot(122)
 
 def animate(i):   
-    a.clear()
-    xlist = []
-    ylist = []
+    aA.clear()
+    aB.clear()
+    xlistA = np.array((1,), dtype = float) #[]
+    ylistA = np.array((1,), dtype = float) #[]
     for elem in lkshr.listA:
-        xlist.append((elem['datetime'] - datetime.datetime(1970,1,1)).total_seconds())
-        ylist.append(elem['data'])
+        np.append(xlistA,(elem['datetime'] - datetime.datetime(1970,1,1)).total_seconds())
+        np.append(ylistA,elem['data'])
 
-    a.plot(xlist,ylist)
+    aA.plot(xlistA,ylistA)
+   # aA.set_title("Input A: %.2fK" % ylistA[-1] )
+
+    xlistB = np.array((1,), dtype = float) #[]
+    ylistB = np.array((1,), dtype = float) #[]
+    for elem in lkshr.listB:
+        np.append(xlistB,(elem['datetime'] - datetime.datetime(1970,1,1)).total_seconds())
+        np.append(ylistB,elem['data'])
+
+    aB.plot(xlistB,ylistB)
+   # aB.set_title("Input B: %.2fK" % ylistB[-1] )
 
 
 class GraphTk(tk.Tk):
@@ -38,6 +52,10 @@ class GraphTk(tk.Tk):
 
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
+        container.grid_rowconfigure(1, weight=1)
+        container.grid_columnconfigure(1, weight=1)
+        container.grid_rowconfigure(2, weight=1)
+        container.grid_columnconfigure(2, weight=1)
 
         #self.frames = {}
 
@@ -59,38 +77,40 @@ class Page(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
-        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
+        #label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        #label.pack(pady=10,padx=10)
 
-        btn1 = tk.Button(self, text = "Measure", command = lambda: lkshr.measureA(1))
-        btn1.pack()
+        btn1 = tk.Button(self, text = "Measure", command = lambda: lkshr.measureB(1))
+        btn1.grid(row = 0, column = 0)
+        #btn1.pack()
         btn2 = tk.Button(self, text = "Stop Measure", command = lambda: lkshr.stopThread())
-        btn2.pack()
+        btn2.grid(row = 1, column = 0)
+        #btn2.pack()
         btn3 = tk.Button(self, text = "Reset Measure", command = lambda: lkshr.configThread())
-        btn3.pack()
+        btn3.grid(row = 2, column = 0)
+        #btn3.pack()
 
-        self.entry1 = tk.Entry(self)
-        self.entry1.pack()
-
-        self.lbl1 = tk.Label(self)
-        self.lbl1.pack()
+        entry1 = tk.Entry(self)
+        #entry1.pack()
+        entry1.grid(row = 0, column = 1)
         
-        btn4 = tk.Button(self, text = "Query", command = lambda: self.runQuery())
-        btn4.pack()
+        v = tk.StringVar()
+        lbl1 = tk.Label(self, textvariable = v)
+        #lbl1.pack()
+        lbl1.grid(row = 2, column = 1)
         
+        btn4 = tk.Button(self, text = "Query", command = lambda: v.set(lkshr.lakestr2str(lkshr.ctrl.query(entry1.get()))))
+        #btn4.pack()
+        btn4.grid(row = 1, column = 1)
         
-        #a.plot([1,2,3,4,5,6,7,8], [1,4,1,5,7,3,9,6])
-        #a.set_title('Test')
-
         canvas = FigureCanvasTkAgg(f, self)
         canvas.show()
-        canvas.get_tk_widget().pack(side = tk.BOTTOM, fill = tk.BOTH, expand = True)
+        #canvas.get_tk_widget().pack(side = tk.LEFT, fill = tk.BOTH, expand = True)
+        canvas.get_tk_widget().grid(row = 0, column = 2, rowspan = 3, columnspan = 2)
+        #canvas._tkcanvas.pack(side = tk.TOP, fill = tk.BOTH, expand = True)
+        canvas._tkcanvas.grid(row = 0, column = 2, rowspan = 3, columnspan = 2)     
 
-        canvas._tkcanvas.pack(side = tk.TOP, fill = tk.BOTH, expand = True)
-     
-    def runQuery(self):
-        str = self.entry1.get
-        self.lbl1.config(text = str)
+        
 # Do I need to put a class that inherits from threading.Thread?
 
 app = GraphTk()
--- TestTkInter/TestTkInter/instruments.py
+++ TestTkInter/TestTkInter/instruments.py
@@ -2,6 +2,7 @@ import pyvisa
 import threading
 import Queue as q
 import datetime
+import numpy as np
 
 rm = pyvisa.ResourceManager()
 
@@ -15,22 +16,34 @@ class lakeshore335():
         self.identity = self.lakestr2str(self.ctrl.query("*IDN?"))
         self.status = self.ctrl.query("*TST?")
         self.queueA = q.Queue()
-        self.listA = []
+        self.listA = []#np.ndarray((1,), dtype = float)
+        self.listB = []#np.ndarray((1,), dtype = float)
         self.thread = threading.Thread()
         self.stop_event = threading.Event()
         
     #methods for threading and measuring    
+
+    def configThread(self):
+        self.stop_event.clear()
+
     def __measureA(self, timestep):
         while (not self.stop_event.is_set()):
             #self.queueA.put({'datetime': datetime.datetime.now(), 'data': self.get_tempA()})
             self.listA.append({'datetime': datetime.datetime.now(), 'data': self.get_tempA()})
             self.stop_event.wait(timestep)
-    
-    def configThread(self):
-        self.stop_event.clear()
 
     def measureA(self, timestep):
         self.thread = threading.Thread(target = self.__measureA, args=(timestep,))
+        self.thread.start()
+
+    def __measureB(self, timestep):
+      while (not self.stop_event.is_set()):
+            #self.queueA.put({'datetime': datetime.datetime.now(), 'data': self.get_tempA()})
+            self.listB.append({'datetime': datetime.datetime.now(), 'data': self.get_tempB()})
+            self.stop_event.wait(timestep)
+    
+    def measureB(self, timestep):
+        self.thread = threading.Thread(target = self.__measureB, args=(timestep,))
         self.thread.start()
 
     def stopThread(self):
