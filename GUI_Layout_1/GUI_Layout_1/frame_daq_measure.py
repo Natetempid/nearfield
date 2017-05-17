1e7fcd3484d9966b3f069046f5cedaea1ff23850
Nathan Thomas <nthomas@caltech.edu>
2017-05-17 09:33:46 -0700

    workingversion

--- GUI_Layout_1/GUI_Layout_1/frame_daq_measure.py
+++ GUI_Layout_1/GUI_Layout_1/frame_daq_measure.py
@@ -15,10 +15,13 @@ class daq_measure_frame(tk.Frame):
     def __init__(self, master, controller, daq9211):
         tk.Frame.__init__(self, master) 
         self.daq9211 = daq9211
-        self.test_btn = ttk.Button(self, text = 'Test', command = lambda: self.test())
-        self.test_btn.pack()
+        #self.test_btn = ttk.Button(self, text = 'Test', command = lambda: self.test())
+        #self.test_btn.pack()
         self.running = False
         self.ani = None
+        self.stopgraph_event = threading.Event()
+        self.stopgraph_event.set()
+
 
         btns = tk.Frame(self)
         btns.pack()
@@ -28,7 +31,7 @@ class daq_measure_frame(tk.Frame):
         self.interval = tk.Entry(btns, width=5)
         self.interval.insert(0, '1')
         self.interval.pack(side=tk.LEFT)
-        self.btn = tk.Button(btns, text='Start', command= lambda: self.on_click())
+        self.btn = ttk.Button(btns, text='Start', command= lambda: self.on_click())
         self.btn.pack(side=tk.LEFT)
 
         #Plotting
@@ -43,27 +46,7 @@ class daq_measure_frame(tk.Frame):
             line, = self.axs[i].plot([], [], lw=2, label = 'A', color = 'b')
             self.lines.append(line)
             #self.axs[i].legend(bbox_to_anchor=(0, 0.02, -.102, -0.102), loc=2, ncol = 2, borderaxespad=0)
-            #self.axs[i].set_title('Channel %d' % i)
-
-        #self.ax0 = self.fig.add_subplot(2,2,1)
-        #self.line0, = self.ax0.plot([], [], lw=2, label = 'A', color = 'b') #I have to address each plot individually. I can't put plots in a list
-        #self.ax0.legend(bbox_to_anchor=(0, 0.02, -.102, -0.102), loc=2, ncol = 2, borderaxespad=0)
-        #self.ax0.set_title('Channel 0')
-        
-        #self.ax1 = self.fig.add_subplot(2,2,2)
-        #self.line1, = self.ax1.plot([], [], lw=2, label = 'A', color = 'b') 
-        #self.ax1.legend(bbox_to_anchor=(0, 0.02, -.102, -0.102), loc=2, ncol = 2, borderaxespad=0)
-        #self.ax1.set_title('Channel 1')
-        
-        #self.ax2 = self.fig.add_subplot(2,2,3)
-        #self.line2, = self.ax2.plot([], [], lw=2, label = 'A', color = 'b') 
-        #self.ax2.legend(bbox_to_anchor=(0, 0.02, -.102, -0.102), loc=2, ncol = 2, borderaxespad=0)
-        #self.ax2.set_title('Channel 2')
-        
-        #self.ax3 = self.fig.add_subplot(2,2,4)
-        #self.line3, = self.ax3.plot([], [], lw=2, label = 'A', color = 'b') 
-        #self.ax3.legend(bbox_to_anchor=(0, 0.02, -.102, -0.102), loc=2, ncol = 2, borderaxespad=0)
-        #self.ax3.set_title('Channel 3')
+            self.axs[i].set_title('Channel %d' % i)
  
 
         self.canvas = FigureCanvasTkAgg(self.fig,self)
@@ -80,12 +63,13 @@ class daq_measure_frame(tk.Frame):
         
 
     def on_click(self):
-        if self.ani is None: #if I haven't initialized the animation through the start command then run self.start
+        if self.ani is None: #if I haven't initialized the animation through the start command then run self.run_tasksetup
             self.run_tasksetup()
             return self.start()
         if self.running: #then the user wants to stop the measurement
-            self.ani.event_source.stop()
+            self.ani = False
             self.daq9211.stop_event.set()
+            self.stopgraph_event.set()
             self.btn.config(text='Start')
         else:
             self.btn.config(text='Stop')
@@ -94,37 +78,17 @@ class daq_measure_frame(tk.Frame):
 
     def start(self):
         self.daq9211.measureAll(int(self.interval.get()))
-        self.ani = animation.FuncAnimation(self.fig, self.update_graph, interval = int(self.interval.get())*1000 + 1, repeat=False)
+        t = threading.Thread(target = self.animation_target)
+        t.start()
+        #self.ani = animation.FuncAnimation(self.fig, self.update_graph, interval = int(self.interval.get())*1000 + 1, repeat=False)
+        self.ani = True
         self.running = True
+
         self.btn.config(text='Stop')
-        self.ani._start()
-
-    #def update_graph(self,i):
-    #    #plot by channel ID
-    #    self.namelist = self.daq9211.channels.keys()
-    #    print self.namelist
-    #    for k in range(0,len(self.namelist)):
-    #        xlist = []
-    #        ylist = []
-    #        ID = self.daq9211.channels[self.namelist[k]].ID
-    #        if ID == 0:
-    #            for elem in self.daq9211.data[self.namelist[k]]:
-    #                xlist.append((elem['datetime'] - datetime.datetime(1970,1,1)).total_seconds())
-    #                ylist.append(elem['data'])
-    #            if xlist and ylist:
-    #                self.line0.set_data(xlist,ylist) #something wrong here
-    #                self.ax0.set_ylim(min(ylist), max(ylist))
-    #                self.ax0.set_xlim(min(xlist), max(xlist))
-    #        #elif ID == 1:
-
-    #        #elif ID == 2:
-
-    #        #elif ID == 3:
-
-    #        else:
-    #            return None
-
-    def update_graph(self,i):
+        #self.anithread = threading.Thread(target = self.ani._start())
+        #self.ani._start()
+    
+    def update_graph(self):#,i):
         #plot by channel ID
         #self.namelist = self.daq9211.channels.keys()
         for k in range(0,len(self.daq9211.channels)):
@@ -138,6 +102,14 @@ class daq_measure_frame(tk.Frame):
             if xlist and ylist:
                 self.axs[ID].set_ylim(min(ylist), max(ylist))
                 self.axs[ID].set_xlim(min(xlist), max(xlist))
+        self.canvas.draw_idle()
+    
+    def animation_target(self):
+        self.stopgraph_event.clear()
+        while(not self.stopgraph_event.is_set()):
+            time.sleep(int(self.interval.get()))
+            self.update_graph()
+        self.stopgraph_event.set() #once animation stops, reset the stop event to trigger again
 
     def run_tasksetup(self):
         for i in range(0,len(self.daq9211.channels)):
